e882fb66c73f0c444d84b105a56e67283fe91aa8
at-duyle <duy.le@asiantech.vn>
2017-08-21 14:30:07 +0700

    Add funtion get_current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,13 @@
 module SessionsHelper
   def login?
-    unless request.headers['HTTP_AUTH_TOKEN'].nil?
-      @current_user = User.find_by(auth_token: request.headers['HTTP_AUTH_TOKEN'])
-    end
     return false if @current_user.blank?
     true
+  end
+
+  def current_user
+    @current_user = nil
+    return @current_user if request.headers['HTTP_AUTH_TOKEN'].nil?
+    @current_user = User.find_by(auth_token: request.headers['HTTP_AUTH_TOKEN'])
   end
 
   def logout
