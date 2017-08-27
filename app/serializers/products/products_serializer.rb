7a262a7ef6006a49a5838031a05f9820eb468f9c
at-duyle <duy.le@asiantech.vn>
2017-08-27 17:45:14 +0700

    Add function list orders of user, add shop for product, config active model serializer

--- app/serializers/products/products_serializer.rb
+++ app/serializers/products/products_serializer.rb
@@ -1,4 +1,5 @@
 class Products::ProductsSerializer < ActiveModel::Serializer
   attributes :id, :name, :producer, :price, :quantity, :description, :categorical_type, :categorical_id, :shop_id, :slug
   has_many :images, serializer: Images::ImagesSerializer
+  has_one :shop, serializer: Shops::ListShopsSerializer
 end
