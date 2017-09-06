e4e80ea1421da320c185cd020bf33f85a6e9c6c3
Duyen Bui <anhduyen1510@gmail.com>
2017-09-06 17:47:49 +0700

    update message for cofirm token

--- app/controllers/register_confirm_controller.rb
+++ app/controllers/register_confirm_controller.rb
@@ -2,18 +2,18 @@ class RegisterConfirmController < ApplicationController
   def update
     user = User.find(confirm_params[:id])
     if user.confirm_at.present?
-      message = { message: 'You have already actived!' }
+      message = { message: 'You have already actived! Let\'s start login' }
     elsif (user.confirm_token.eql? user[:confirm_token]) &&
           (user.confirm_send_at + 3.days > Time.now)
       user.skip_password_validation = true
       user.update(confirm_at: Time.now)
-      message = { message: 'You actived successfully!' }
+      message = { message: 'You actived successfully! Let\'s start login' }
     else
-      message = { message: 'Please get a new token!' }
+      message = { message: 'The confirm token is overdue. Please get a new token!' }
     end
     render json: message
   rescue
-    error = { errors: 'Data not found' }
+    error = { errors: 'User not found' }
     render json: error, status: 404
   end
 
