e3886b66e66052feeae7a326c72309d38f6a9365
Duyen Bui <anhduyen1510@gmail.com>
2017-09-06 16:44:40 +0700

    update url confirm email when register

--- app/mailers/register_mailer.rb
+++ app/mailers/register_mailer.rb
@@ -1,7 +1,7 @@
 class RegisterMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url = 'http://0.0.0.0:4200/login'
+    @url = "http://127.0.0.1:4200/register/accept/#{@user.id}/#{@user.confirm_token}"
     mail(to: @user.email, subject: 'Welcome to Super Shop')
   end
 end
