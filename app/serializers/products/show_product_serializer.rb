fb94b76c5763bf1884c51c34ff70ef575fce9f57
Duyen Bui <anhduyen1510@gmail.com>
2017-09-09 01:28:28 +0700

    update show product information

--- app/serializers/products/show_product_serializer.rb
+++ app/serializers/products/show_product_serializer.rb
@@ -2,5 +2,5 @@ class Products::ShowProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :producer, :price, :quantity, :description, :slug
   has_many :images, serializer: Images::ImagesSerializer
   has_one :shop, serializer: Shops::ListShopsSerializer
-  # has_many :comments, serializer: Comments::CommentsSerializer
+  has_many :comments, serializer: Comments::CommentsSerializer
 end
